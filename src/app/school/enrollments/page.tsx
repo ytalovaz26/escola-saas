2c483ece536f3c7c58a0d9fd5c0d9eab58361135
Ytalo Vaz <ytalovaz26@gmail.com>
2026-03-27 11:08:22 -0300

    fix: enrollments suspense + build fix

--- src/app/school/enrollments/page.tsx
+++ src/app/school/enrollments/page.tsx
@@ -1,7 +1,7 @@
-"use client";
-
 import { Suspense } from "react";
 import PageContent from "./PageContent";
+
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   return (
